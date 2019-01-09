2777b6edc8efd6444f1679f1540dc05117e5d362
Rama <iammastercloud@gmail.com>
2019-01-09 20:33:23 +0800

    de bayesian final project bug : department was token as integer but not string

---
+++ 貝氏資料分析(Introduction to Bayesian Data Analysis)/final_project/preprocessing/Jags-Ybinom-XnomSsubjCcat-MbinomBetaOmegaKappa-Example.R
@@ -0,0 +1,47 @@
+# Example for Jags-Ybinom-XnomSsubjCcat-MbinomBetaOmegaKappa.R 
+#------------------------------------------------------------------------------- 
+# Optional generic preliminaries:
+graphics.off() # This closes all of R's graphics windows.
+rm(list=ls())  # Careful! This clears all of R's memory!
+#------------------------------------------------------------------------------- 
+# Read the data 
+myData = read.csv("~/Desktop/preprocessing/ScoreAverage.csv")
+#------------------------------------------------------------------------------- 
+# Load the relevant model into R's working memory:
+source("~/Desktop/preprocessing/Jags-Ybinom-XnomSsubjCcat-MbinomBetaOmegaKappa.R")
+#------------------------------------------------------------------------------- 
+# Optional: Specify filename root and graphical format for saving output.
+# Otherwise specify as NULL or leave saveName and saveType arguments 
+# out of function calls.
+fileNameRoot = "BattingAverage-POST-" 
+graphFileType = "eps" 
+#------------------------------------------------------------------------------- 
+# Generate the MCMC chain:
+startTime = proc.time()
+mcmcCoda = genMCMC( data=myData , 
+                    zName="Down", NName="Semester_Count", sName="S_ID", cName="Department",
+                    numSavedSteps=11000 , saveName=fileNameRoot ,
+                    thinSteps=20 )
+stopTime = proc.time()
+elapsedTime = stopTime - startTime
+show(elapsedTime)
+#------------------------------------------------------------------------------- 
+# Display diagnostics of chain, for specified parameters:
+parameterNames = varnames(mcmcCoda) # get all parameter names for reference
+for ( parName in c("omega[1]","omegaO","kappa[1]","kappaO","theta[1]") ) { 
+  diagMCMC( codaObject=mcmcCoda , parName=parName , 
+                saveName=fileNameRoot , saveType=graphFileType )
+}
+#------------------------------------------------------------------------------- 
+# Get summary statistics of chain:
+summaryInfo = smryMCMC( mcmcCoda , compVal=NULL , 
+                        diffSVec=c(75,156, 159,844) , 
+                        diffCVec=c(1,2,3) , 
+                        compValDiff=0.0 , saveName=fileNameRoot )
+# Display posterior information:
+plotMCMC( mcmcCoda , data=myData , 
+          zName="Down", NName="Semester_Count", sName="S_ID", cName="Department",
+          compVal=NULL ,
+          compValDiff=0.0, #ropeDiff = c(-0.05,0.05) ,
+          saveName=fileNameRoot , saveType=graphFileType )
+#------------------------------------------------------------------------------- 
